d7ae6f10aabba78fee77aca099270d49a1cd8a95
santiagofaginoli <CORREO@example.com>
2024-09-01 12:20:14 -0300

    first commit3

--- src/app/api/products/update-stock/route.ts
+++ src/app/api/products/update-stock/route.ts
@@ -1,60 +1,57 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import Product from "@/app/models/products";
 import dbConnect from "@/app/lib/dbConnect";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function POST(req: NextRequest) {
   await dbConnect();
 
-  if (req.method === "POST") {
-    const { cartItems } = req.body;
+  const { cartItems } = await req.json();
 
-    try {
-      for (const item of cartItems) {
-        const product = await Product.findById(item._id);
+  try {
+    for (const item of cartItems) {
+      const product = await Product.findById(item._id);
 
-        if (!product) {
-          return res
-            .status(404)
-            .json({ message: `Producto no encontrado para ID ${item._id}` });
-        }
-
-        // Encontrar el color y tamaño correctos en el stock del producto
-        const stockColor = product.stock.find(
-          (s: any) => s.color === item.color
+      if (!product) {
+        return NextResponse.json(
+          { message: `Producto no encontrado para ID ${item._id}` },
+          { status: 404 }
         );
-
-        if (!stockColor) {
-          return res
-            .status(400)
-            .json({ message: `No se encontró stock para color ${item.color}` });
-        }
-
-        const stockSizeIndex = stockColor.size.indexOf(item.size);
-
-        if (stockSizeIndex === -1) {
-          return res
-            .status(400)
-            .json({
-              message: `No se encontró tamaño ${item.size} para el color ${item.color}`,
-            });
-        }
-
-        // Restar la cantidad comprada del stock disponible
-        stockColor.quantity[stockSizeIndex] -= item.quantity;
-
-        // Guardar el producto actualizado en la base de datos
-        await product.save();
       }
 
-      res.status(200).json({ message: "Stock actualizado con éxito" });
-    } catch (error) {
-      console.error("Error actualizando el stock:", error);
-      res.status(500).json({ message: "Error al actualizar el stock" });
+      // Encontrar el color y tamaño correctos en el stock del producto
+      const stockColor = product.stock.find((s: any) => s.color === item.color);
+
+      if (!stockColor) {
+        return NextResponse.json(
+          { message: `No se encontró stock para color ${item.color}` },
+          { status: 400 }
+        );
+      }
+
+      const stockSizeIndex = stockColor.size.indexOf(item.size);
+
+      if (stockSizeIndex === -1) {
+        return NextResponse.json(
+          {
+            message: `No se encontró tamaño ${item.size} para el color ${item.color}`,
+          },
+          { status: 400 }
+        );
+      }
+
+      // Restar la cantidad comprada del stock disponible
+      stockColor.quantity[stockSizeIndex] -= item.quantity;
+
+      // Guardar el producto actualizado en la base de datos
+      await product.save();
     }
-  } else {
-    res.status(405).json({ message: "Método no permitido" });
+
+    return NextResponse.json({ message: "Stock actualizado con éxito" });
+  } catch (error) {
+    console.error("Error actualizando el stock:", error);
+    return NextResponse.json(
+      { message: "Error al actualizar el stock" },
+      { status: 500 }
+    );
   }
 }
